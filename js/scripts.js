b32769df631749589a61eaace0e968c427651c82
Sharon Olago <adino@lisheliving.com>
2018-12-13 13:57:19 +0300

    added container and two classes for the two dice

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 // Function to make the dice pick random numbers
 var iRandom;
-iRandom = Math.random ();
-document.writeIn (iRandom);
+var iRoll= {};
+var i;
+
+iRol = prompt ("Roll the dice", 6);
+
+for (i=1; i<=iRoll; i++){
+  iRandom = Math.cel (Math.random()*6);
+  document.writeIn(iRandom);
+}

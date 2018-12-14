59d81e8fc27d5721a8260ecaaef0f251635f6146
Sharon Olago <adino@lisheliving.com>
2018-12-14 16:16:38 +0300

    added function for computer turna nd count

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ $(document).ready(function(){
 var userScore=0;
 var compScore=0;
 
+var compTurn = 0;
+var compCount = 0;
+
+function userTurn (){
+  var yourTurn=
+}
+
+function
+
 $("#checkAnswer").click(function(){
 
 var iRandom;

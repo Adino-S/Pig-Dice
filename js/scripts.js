5735d7d1fba6dc93dcb7f7aeec6cf82591d33823
Sharon Olago <adino@lisheliving.com>
2018-12-17 17:38:52 +0300

    added changes to js script from gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,15 @@
-$(document).ready(function() {
-    var userScore = 0;
-    var compScore = 0;
-    var compTurn = 0;
-    var compCount = 0;
-  }
+// Business Logic
 
-  function userTurn() {
-    var currentTurn = 0;
-    do {
-      text += "The number is " + i;
-      i++;
-      document.write();
-    }
-    while (i < 5);
-  })
-  var count = 0;
-})
-
-function compTurn(){
-  var currentTurn = 0;
-  do {
-    text += "The number is " + i;
-    i++;
-    document.write();
-  }
-  while (i < 5);
-  }
-  var count = 0;
+var myRandom = function() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-
-
-
-$("#checkAnswer").click(function() {
-
-      var iRandom;
-      var scoreA = () {
-          $("#scoreA").click(function() iRoll = {}
-          }
-
-
-          for (i = 1; i <= iRoll; i++) {
-            iRandom = Math.cel(Math.random() * 6);
-            document.writeIn(iRandom);
-          }
+// User Interface
+$(document).ready(function() {
+  $(".btn-1").click(function() {
+    $("#currentCount").val(myRandom);
+  })
+  $(".btn-2").click(function() {
+    $("#computerCount").val(myRandom);
+  })
+})
